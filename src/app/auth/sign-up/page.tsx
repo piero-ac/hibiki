83ff98abf81105229ccb7c1a6b051780cd715da0
Piero Coronado Flores <pacoronado01@gmail.com>
2026-07-02 19:24:35 -0400

    feat: implement async login and sign-up page redirection based on user claims

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,6 +1,14 @@
 import SignUpForm from "@/components/auth/sign-up-form";
+import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
 
-export default function Page() {
+export default async function Page() {
+	const supabase = await createClient();
+	const { data } = await supabase.auth.getClaims();
+
+	if (data?.claims) {
+		redirect("/protected");
+	}
 	return (
 		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
 			<div className="w-full max-w-sm">
